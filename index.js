757a4e1322b21a297ffe47648853a2b7447d990c
onlymohmmed3 <mm86434.6.80@gmail.com>
2024-08-11 22:36:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,16 @@ client.on('ready', () => {
 }); 
 //https://ra3dstudio.com CopyRight Codes
 client.login(process.env.token);
+
+const express = require("express");
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get("/", (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`);
+});
